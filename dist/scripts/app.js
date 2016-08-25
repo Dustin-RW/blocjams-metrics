0daf42313ab0f01438e661fd3b65f4cd8c22dad2
Dustin <dustinwaggoner@comcast.net>
2016-08-25 13:05:14 -0700

    checkpoint complete, angular config working

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,1 +1,27 @@
-angular.module('blocJams', []); //1rst arg is name of module, 2nd is empty array which injects dependencies
+//angular.module('blocJams', []); //1rst arg is name of module, 2nd is empty array which injects dependencies
+//angular.module('blocJams', ['ui.router']);
+(function() {
+    function config($stateProvider, $locationProvider) {
+        $locationProvider
+            .html5Mode({ //hashBang URLs are disabled
+            enabled: true,
+            requireBase: false
+        });
+
+        $stateProvider
+            .state('landing', {
+                url: '/',
+                templateUrl: '/templates/landing.html'
+            })
+            .state('album', {
+                url: '/album',
+                templateUrl: '/templates/album.html'
+            });
+      }
+
+
+
+    angular
+        .module('blocJams', ['ui.router'])
+        .config(config);
+})();
