4a150a29a84c68dbb5d3f6b1099bb8c8a406e3a8
Dustin <dustinwaggoner@comcast.net>
2016-08-30 19:06:06 -0700

    service part 2 checkpoint complete

--- dist/scripts/controllers/AlbumCtrl.js
+++ dist/scripts/controllers/AlbumCtrl.js
@@ -1,11 +1,12 @@
 (function() {
 
-  function AlbumCtrl(Fixtures) {
+  function AlbumCtrl(Fixtures, SongPlayer) {
     this.albumData = Fixtures.getAlbum();
+    this.songPlayer = SongPlayer; //holds the service and makes the service accessible within the Album View
   };
 
   angular
     .module('blocJams')
-    .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
+    .controller('AlbumCtrl', ['Fixtures', 'SongPlayer', AlbumCtrl]);
 
 })();
