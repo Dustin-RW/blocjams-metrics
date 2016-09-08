018bdeb79cab7514c59f54bcf018c1cc2672d39d
Dustin <dustinwaggoner@comcast.net>
2016-09-08 10:14:13 -0700

    filter modified to utilize buzz toTimer method

--- app/scripts/filters/timecode.js
+++ app/scripts/filters/timecode.js
@@ -1,24 +1,26 @@
 (function() {
   function timecode() {  //filter function must return another function which takes at least on argument, the input filter
     return function(seconds) {
-      var seconds = Number.parseFloat(seconds);
+      // var seconds = Number.parseFloat(seconds);
+      //
+      var output = buzz.toTimer(seconds);
 
       if (Number.isNaN(seconds)) {
         return '-:--';
       }
-
-      var wholeSeconds = Math.floor(seconds);
-      var minutes = Math.floor(wholeSeconds / 60);
-      var remainingSeconds = wholeSeconds % 60;
-
-      var output = minutes + ':';
-
-      if (remainingSeconds < 10) {
-          output += '0';
-      }
-
-      output += remainingSeconds;
-
+      //
+      // var wholeSeconds = Math.floor(seconds);
+      // var minutes = Math.floor(wholeSeconds / 60);
+      // var remainingSeconds = wholeSeconds % 60;
+      //
+      // var output = minutes + ':';
+      //
+      // if (remainingSeconds < 10) {
+      //     output += '0';
+      // }
+      //
+      // output += remainingSeconds;
+      //
       return output;
     };
   };
